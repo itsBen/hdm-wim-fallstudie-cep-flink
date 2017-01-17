8c335a853a39c13b3274b2b2272a98587e7648a6
itsBen <benedikt.zneb@gmail.com>
2017-01-17 18:53:21 +0100

    Added Message class

---
+++ src/main/java/de/hdm/wim/classes/Message.java
@@ -0,0 +1,58 @@
+package de.hdm.wim.classes;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ben on 16.01.2017.
+ */
+public class Message {
+
+    private String message;
+    public List<String> tokens = new ArrayList<String>();
+    private Participant sender;
+    private LocalDateTime timestamp;
+
+    public Message(List<String> tokens, Participant sender, LocalDateTime timestamp) {
+        this.tokens = tokens;
+        this.sender = sender;
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<String> getTokens() {
+        return tokens;
+    }
+
+    public void setTokens(List<String> tokens) {
+        this.tokens = tokens;
+    }
+
+    public void addToken(String token) {
+        this.tokens.add(token);
+    }
+
+    public Participant getSender() {
+        return sender;
+    }
+
+    public void setSender(Participant sender) {
+        this.sender = sender;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
