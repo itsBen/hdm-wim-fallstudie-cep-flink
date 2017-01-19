2387c85d707d8e5caa4339c09b997c44ef77ec9c
itsBen <benedikt.zneb@gmail.com>
2017-01-19 13:39:43 +0100

    Clean + moved patterns to own folder

--- src/main/java/de/hdm/wim/CEPSpeechTokens.java
+++ src/main/java/de/hdm/wim/CEPSpeechTokens.java
@@ -1,11 +1,11 @@
 package de.hdm.wim;
 
-import de.hdm.wim.source.EventSource;
+import de.hdm.wim.events.MessageEvent;
+import de.hdm.wim.patterns.SenderPattern;
+import de.hdm.wim.patterns.TestPattern;
+import de.hdm.wim.source.MessageEventSource;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.environment.LocalStreamEnvironment;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-
-import java.util.List;
 
 /**
  * Created by Ben on 16.01.2017.
@@ -14,37 +14,57 @@ public class CEPSpeechTokens {
 
         public static void main(String[] args) throws Exception {
 
-
             final StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment();
+            env.setParallelism(1); // set Parallelism to 1 Task, to get chat order right
 
-            final LocalStreamEnvironment env2 = LocalStreamEnvironment.createLocalEnvironment();
+            MessageEventSource evtSrc = new MessageEventSource();
 
-
-
-
-            EventSource evtSrc = new EventSource();
-
-
-            DataStream<List<List<String>>> messageStream2 = env2
+            // create a DataStream of MessageEvent from our source
+            DataStream<MessageEvent> messageStream = env
                     .addSource(evtSrc);
 
 
-            DataStream<List<List<String>>> messageStream = env
-                    .addSource(evtSrc);
+            TestPattern testPattern = new TestPattern();
+            testPattern.run(env,messageStream);
+
+            SenderPattern senderPattern = new SenderPattern();
+            senderPattern.run(env,messageStream);
 
 
-            System.out.print("blubb");
+            /*
+            // Warning pattern: Two consecutive temperature events whose temperature is higher than the given threshold
+            // appearing within a time interval of 10 seconds
+            Pattern<MessageEvent, ?> projectPattern = Pattern.<MessageEvent>begin("first")
+                    .subtype( ProjectEvent.class )
+                    .where( evt -> evt.getTokens().contains("concerning") );
+                            //.stream()
+                            //.anyMatch( token -> token.equals("concerning") )
+                    //);
+//                            .anyMatch( token -> (token.equals("concerning") || token.equals("regarding") ) && token.equals("project") ));
 
+            // Create a pattern stream from our project pattern
+            PatternStream<MessageEvent> projectPatternStream = CEP.pattern(
+                    messageStream.keyBy("_messageId"),
+                    projectPattern);
 
+            // Generate ProjectEvents for each matched project pattern
+            DataStream<ProjectEvent> projectStream = projectPatternStream.select(
+                    (Map<String, MessageEvent> pattern) -> {
+                        ProjectEvent projectEvent = (ProjectEvent) pattern.get("first");
 
+                        return projectEvent;
+                    }
+            );
 
             // print to stdout
+            //projectStream.print();
+
+
+*/
+            // print message stream
             messageStream.print();
-            messageStream2.print();
 
-            env.execute("CEP monitoring job");
 
-            env2.execute("CEP monitoring job2");
-
+            env.execute("CEP chat stream job");
         }
 }
